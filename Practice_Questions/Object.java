95fa15723daf0bcafbe09b701324a1c3c9b0defe
Shagun Upreti <shagunupret1237@gmail.com>
2024-11-14 05:40:33 +0000

    object

---
+++ Practice_Questions/Object.java
@@ -0,0 +1,12 @@
+class Object{
+    
+    String color;
+    int age;
+    public static void main(String[] args){
+        Object buzo=new Object();
+        buzo.color="black";
+        buzo.age=10;
+
+        System.out.println(buzo.color+" "+buzo.age);
+    }
+}
