baab04e527d14624c2748a7a7b9e55ab6973a81b
Shagun Upreti <shagunupret1237@gmail.com>
2024-11-19 04:44:58 +0000

    Prime number till N

---
+++ Practice_Questions/PrimeTillNumberN.java
@@ -0,0 +1,20 @@
+class PrimeTillNumberN{
+
+    public static void main(String[] args){
+
+        int num=100;
+       
+        for(int i=1;i<=num;i++){
+            int count=0;
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
