7aac1339b83f452f19c34c69351f9633ed7292aa
shagun-upreti <160765525+shagun-upreti@users.noreply.github.com>
2024-11-25 06:07:59 +0000

    star pattern 4

---
+++ Practice_Questions/Pattern4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Pattern4{
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int st=n;
+        int sp=0;
+        for(int i=1;i<=n;i++){
+
+           
+            for(int j=1;j<=sp;j++){
+                System.out.print("\t");
+            }
+            for(int j=1;j<=st;j++){
+                System.out.print("*\t");
+            }
+        st--;
+        sp++;
+        System.out.println("");
+        }
+    }
+}
