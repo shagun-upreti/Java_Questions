a4a4a10d28109c6dce223d5d1fafc88323589b72
shagun-upreti <160765525+shagun-upreti@users.noreply.github.com>
2024-12-12 04:13:33 +0000

    Base To Decimal

--- Practice_Questions/BaseToDecimal.java
+++ Practice_Questions/BaseToDecimal.java
@@ -1,9 +1,28 @@
+// class BaseToDecimal{
+
+//     public static void main(String[] args){
+
+//         String binaryNumber = "111001";
+//         int decimalValue = Integer.parseInt(binaryNumber, 2);
+//         System.out.println("Binary " + binaryNumber + " to Decimal is " + decimalValue);
+//     }
+// }
+import java.util.Scanner;
 class BaseToDecimal{
 
     public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int b=scn.nextInt();
+        int rv=0;
+        int p=1;
+        while(n>0){
+            int dig=n%10;
+            n=n/10;
+            rv+=dig*p;
 
-        String binaryNumber = "111001";
-        int decimalValue = Integer.parseInt(binaryNumber, 2);
-        System.out.println("Binary " + binaryNumber + " to Decimal is " + decimalValue);
+            p=p*b;
+        }
+        System.out.println(rv);
     }
 }
