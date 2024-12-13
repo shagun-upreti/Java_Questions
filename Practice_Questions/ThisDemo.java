d943e9eeb38a041a6ff0c3ae98aaf115452f7071
shagun-upreti <160765525+shagun-upreti@users.noreply.github.com>
2024-12-13 09:07:34 +0000

    this() keyword can be used to pass as an argument in the method call

--- Practice_Questions/ThisDemo.java
+++ Practice_Questions/ThisDemo.java
@@ -1,22 +1,42 @@
+// class ThisDemo{          --------------------------this() keyword can invoke current class constructors----------------------
+
+
+//     ThisDemo(){
+
+//         System.out.println("no agrs constructor");
+//     }
+
+//     ThisDemo(int a){
+        
+//         System.out.println("Parameterised constructor");
+
+
+//     }
+//     public static void main(String[] args){
+
+//      ThisDemo td1=new ThisDemo();
+//      ThisDemo tf2=new ThisDemo(10);
+     
+    
+//     }
+// }
+
+
+//-------------------------this() keyword can be used to pass as an argument in the method call------------------------------
 class ThisDemo{
 
+    void m1(ThisDemo hi){
 
-    ThisDemo(){
-
-        System.out.println("no agrs constructor");
+        System.out.println("method1");
     }
 
-    ThisDemo(int a){
-        
-        System.out.println("Parameterised constructor");
+    void m2(){
 
-
+        m1(this);
     }
     public static void main(String[] args){
 
-     ThisDemo td1=new ThisDemo();
-     ThisDemo tf2=new ThisDemo(10);
-     
-    
+        ThisDemo td=new ThisDemo();
+        td.m2();
     }
 }
